0e5d96ce35ab025a95e08820d04c7d8a3bfe727c
Allen Lee <looplee@msn.com>
2024-05-26 14:43:21 +0800

    Create checking in tests #3

--- test/PiggyLegacy.js
+++ test/PiggyLegacy.js
@@ -1,6 +1,6 @@
 const {
     loadFixture,
-    time
+    time,
   } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const chai = require('chai');
 chai.use(require('chai-luxon'));
@@ -32,4 +32,43 @@ describe("PiggyLegacy", function() {
         });
     });
 
+    describe("Checking in", () => {
+        it("should check in tomorrow when the owner calls the checkIn function tomorrow (T + 1)", async function() {
+            const { legacy, initialAmount } = await loadFixture(deployLegacyFixture);
+
+            const tomorrowUTC = DateTime.utc().plus({ days: 1 });
+            await time.increaseTo(Math.round(tomorrowUTC.toSeconds()));
+            await legacy.checkIn();
+            const lastCheckInUTC = DateTime.fromSeconds(Number(await legacy.lastCheckInTime())).setZone("UTC");
+            
+            expect(lastCheckInUTC).to.be.sameDate(tomorrowUTC);
+            expect(await legacy.canWithdraw()).to.be.false;
+            expect(await ethers.provider.getBalance(legacy.target)).to.equal(initialAmount);
+        });
+
+        it("should check in and receive the funds tomorrow when the owner calls the checkIn function with new funds tomorrow (T + 1)", async function() {
+            const { legacy, initialAmount } = await loadFixture(deployLegacyFixture);
+
+            const tomorrowUTC = DateTime.utc().plus({ days: 1 });
+            await time.increaseTo(Math.round(tomorrowUTC.toSeconds()));
+            const additionalAmount = 2_000_000_000;
+            await legacy.checkIn({value: additionalAmount});
+            const lastCheckInUTC = DateTime.fromSeconds(Number(await legacy.lastCheckInTime())).setZone("UTC");
+            
+            expect(lastCheckInUTC).to.be.sameDate(tomorrowUTC);
+            expect(await legacy.canWithdraw()).to.be.false;
+            expect(await ethers.provider.getBalance(legacy.target)).to.equal(initialAmount + additionalAmount);
+        });
+
+        it("should revert when non-owner checks in", async function() {
+            const { legacy, beneficiary } = await loadFixture(deployLegacyFixture);
+
+            const lastCheckInTime = await legacy.lastCheckInTime();
+            const contractBalance = await ethers.provider.getBalance(legacy.target);
+
+            await expect(legacy.connect(beneficiary).checkIn()).to.be.revertedWith("Owner only");
+            expect(await legacy.lastCheckInTime()).to.equal(lastCheckInTime);
+            expect(await ethers.provider.getBalance(legacy.target)).to.equal(contractBalance);
+        });
+    });
 });
